5e307e1a132a519a26d6208e2bdd60dd3a8ca032
vlakoosh <vlakoosh@gmail.com>
2024-02-21 12:58:51 +0100

    background change

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let window_width = window.innerWidth;
 
 canvas.height = window_height;
 canvas.width = window_width;
-canvas.style.background = "green";
+canvas.style.background = "whitesmoke";
 
 
 
